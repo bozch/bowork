c816b371cd45700b2a0b3dfcd280889d6614236f
bozhencheng <bozhencheng@gyyx.cn>
2017-06-16 21:58:38 +0800

    init the bowork workspace.

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'bowork'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
